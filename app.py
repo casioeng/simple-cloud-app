21f6e1d513128a9490bb732db71ae961551c3757
casioeng <casioeng27@gmail.com>
2025-11-25 10:44:47 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is my cloud application deployed on Render!"
+    return "This is my cloud application deployed on Render! 22141207"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
